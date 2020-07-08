d494169c8c040800932de78b0da1b949caf0daa9
Mason Slover <masonslover@gmail.com>
2020-07-08 16:17:02 -0400

    images

---
+++ script.js
@@ -0,0 +1,59 @@
+// let canvas = document.getElementById("canvas");
+// let ctx = canvas.getContext("2d");
+// ctx.fillStyle = '#FF0000';
+
+
+var mousePressed = false;
+var lastX, lastY;
+var ctx;
+let canvas = document.getElementById("canvas");
+
+function InitThis() {
+
+
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+
+
+    ctx = document.getElementById('canvas').getContext("2d");
+
+    $('#canvas').mousedown(function (e) {
+        mousePressed = true;
+        Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
+    });
+
+    $('#canvas').mousemove(function (e) {
+        if (mousePressed) {
+            Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
+        }
+    });
+
+    $('#canvas').mouseup(function (e) {
+        mousePressed = false;
+    });
+    $('#canvas').mouseleave(function (e) {
+        mousePressed = false;
+    });
+}
+
+function Draw(x, y, isDown) {
+    if (isDown) {
+        ctx.beginPath();
+        ctx.strokeStyle = $('#selColor').val();
+        ctx.lineWidth = $('#selWidth').val();
+        ctx.lineJoin = "round";
+        ctx.moveTo(lastX, lastY);
+        ctx.lineTo(x, y);
+        ctx.closePath();
+        ctx.stroke();
+    }
+    lastX = x; lastY = y;
+}
+
+function clearArea() {
+    // Use the identity matrix while clearing the canvas
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
