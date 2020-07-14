8883773e784f781f00a8a4b141805290526e6bad
Mason Slover <masonslover@gmail.com>
2020-07-14 12:25:49 -0400

    Update

---
+++ README.md
@@ -0,0 +1,2 @@
+# Etch-A-Sketch
+An experimental project to create a skeuomorphic Etch-A-Sketch in the browser. This project responsivly adapts to the device being used. Click the top border to clear!
--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-// let canvas = document.getElementById("canvas");
-// let ctx = canvas.getContext("2d");
-// ctx.fillStyle = '#FF0000';
 
 
-var mousePressed = false;
-var lastX, lastY;
-var ctx;
+let mousePressed = false;
+let lastX, lastY;
+let ctx;
 let canvas = document.getElementById("canvas");
+
+
+document.ontouchmove = function(e){ e.preventDefault(); }
+
+
+
 
 function InitThis() {
 
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+
+
+    // canvas.width = window.innerWidth;
+    canvas.width = document.body.getBoundingClientRect().width;
+    // canvas.height = window.innerHeight;
+    canvas.height = document.body.getBoundingClientRect().height;
 
     ctx = document.getElementById('canvas').getContext("2d");
 
@@ -21,21 +28,43 @@ function InitThis() {
     });
 
     $('#canvas').mousemove(function (e) {
-        if (mousePressed) {
+        // if (mousePressed) {
             Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
-        }
+        // }
     });
 
     $('#canvas').mouseup(function (e) {
         mousePressed = false;
+        Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
     });
     $('#canvas').mouseleave(function (e) {
         mousePressed = false;
+        // Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
     });
 }
 
+
+
+
+canvas.ontouchmove = function(event){
+    event.preventDefault();
+    let newx = event.touches[0].clientX;
+    let newy = event.touches[0].clientY;
+    ctx.lineTo(newx,newy);
+
+}
+
+canvas.addEventListener("touchmove", function (e) {
+    var touch = e.touches[0];
+    var mouseEvent = new MouseEvent("mousemove", {
+        clientX: touch.clientX,
+        clientY: touch.clientY
+    });
+    canvas.dispatchEvent(mouseEvent);
+}, false);
+
 function Draw(x, y, isDown) {
-    if (isDown) {
+    // if (isDown) {
         ctx.beginPath();
         ctx.strokeStyle = $('#selColor').val();
         ctx.lineWidth = $('#selWidth').val();
@@ -44,7 +73,7 @@ function Draw(x, y, isDown) {
         ctx.lineTo(x, y);
         ctx.closePath();
         ctx.stroke();
-    }
+    // }
     lastX = x; lastY = y;
 }
 
@@ -53,3 +82,9 @@ function clearArea() {
     ctx.setTransform(1, 0, 0, 1, 0, 0);
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 }
+
+let btn = document.getElementById("top").addEventListener("click", function () {
+    clearArea()
+    console.log("Clicked border!");
+});
+
